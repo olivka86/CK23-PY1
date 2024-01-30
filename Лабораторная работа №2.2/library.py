ef9c3e8088a14a3e674d37600494c5bb8c24f2a5
great <greatolivka@gmail.ru>
2024-01-30 21:09:42 +0300

    lab2.1

---
+++ Лабораторная работа №2.2/library.py
@@ -0,0 +1,50 @@
+from typing import List
+from pydantic import BaseModel
+
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book(BaseModel):
+    id_: int
+    name: str
+    pages: int
+
+
+class Library(BaseModel):
+    books: List[Book] = []
+
+    def get_next_book_id(self):
+        if self.books:
+            return len(self.books) + 1
+        return 1
+
+    def get_index_by_book_id(self, id_: int) -> int:
+        for index, value in enumerate(self.books):
+            if value.id_ == id_:
+                return index
+        raise ValueError('Книги с запрашиваемым id не существует')
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
